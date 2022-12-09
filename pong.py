88d472973618ed35f1344f447e3ddb9c4c06cb1f
lawilmoth <lwilmoth@eriesd.org>
2022-12-09 10:18:53 -0500

    ball is working

--- pong.py
+++ pong.py
@@ -1,80 +1,110 @@
 import pygame
-pygame.init()
-pygame.display.set_caption('Pong')
 
-WIDTH, HEIGHT = 700, 500
+pygame.init()
+pygame.display.set_caption("Pong")
+
+
+############# CONSTATNSTS ################### 
+WIDTH, HEIGHT = 700, 500 
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100 
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-
 FPS = 60
-PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 
+##############CLASSES#########################
 class Paddle:
     COLOR = "White"
     VEL = 4
-    def __init__(self, x, y, width, height):
+
+    def __init__(self, x, y , width, height):
         self.x = x
         self.y = y
-        self.width = width
         self.height = height
-
+        self.width = width
+    
     def draw(self, win):
         pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
 
     def move(self, up=True):
-        if up:
+        if up: 
             self.y -= self.VEL
         else:
             self.y += self.VEL
 
 
+class Ball:
+    COLOR = "White"
+    MAX_VEL = 5
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.x_vel = self.MAX_VEL
+        self.y_vel = 0
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+
+
+
+###########################MAIN LOOP##########################
+def main(): #Main loop of the game
+    run = True
+
+    clock = pygame.time.Clock()
+
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT) 
+    right_paddle = Paddle(WIDTH -10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT) 
+
+    ball = Ball(WIDTH//2, HEIGHT//2, 7)
+
+    while run:
+        clock.tick(FPS)
+        draw(WIN, [left_paddle, right_paddle], ball)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys, left_paddle, right_paddle)
+        ball.move()
+
+        
+    pygame.quit()
+##################################################################   
+##############FUNCTIONS########################
+
 def handle_paddle_movement(keys, left_paddle, right_paddle):
-    if keys[pygame.K_w] and left_paddle.y -left_paddle.VEL >= 0:
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0: 
         left_paddle.move(up = True)
-    if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
+    if keys[pygame.K_s] and left_paddle.y +left_paddle.VEL <= HEIGHT - left_paddle.height:
         left_paddle.move(up = False)
 
-    if keys[pygame.K_UP]:
-        right_paddle.move(up = True)
-    if keys[pygame.K_DOWN]:
-        right_paddle.move(up = False)
 
-def draw(win, paddles):  #1
-    win.fill('Black')  #1
-
+def draw(win, paddles, ball):
+    win.fill("Black")
 
     for paddle in paddles:
         paddle.draw(win)
+
+    for i in range(10, HEIGHT, HEIGHT//20):
+        if i % 2 == 1:
+            continue
+        pygame.draw.rect(win, "White",(WIDTH//2-5, i, 10, HEIGHT//20))
+
+    ball.draw(win)
+    #Update should always be last when we draw.
     pygame.display.update()
 
-
-def main(): #1
-    run = True #1
-
-
-    clock = pygame.time.Clock()
-    left_paddle = Paddle(10, HEIGHT//2 -PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT )
-    right_paddle = Paddle(WIDTH-10 -PADDLE_WIDTH, HEIGHT//2 -PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT )
-    
-    
-    while run: #1
-        clock.tick(FPS)
-        draw(WIN, [left_paddle, right_paddle])
-        
-        
-        for event in pygame.event.get(): #1
-            if event.type == pygame.QUIT: #1
-                run = False #1
-                break #1
-        
-        
-        keys = pygame.key.get_pressed()
-        handle_paddle_movement(keys, left_paddle, right_paddle)
-
-    pygame.quit() #1
-
-
-
-if __name__ == '__main__': #1
+ 
+ 
+if __name__ == '__main__':
     main()
-
